f26d5354de2e8f24444a7ed42903110bfb2cb281
bmcgruddy <rdd6afjwo@mozmail.com>
2023-10-16 19:51:27 +1100

    Seperate ExpanderFunction into 3 different modes, create initial scale to zoom implementation.

--- canvas_expander/canvas_expander.py
+++ canvas_expander/canvas_expander.py
@@ -2,9 +2,19 @@
 
 from krita import Extension
 from .expander_function import ExpanderFunction
+from .scale_to_zoom_function import ScaleToZoomFunction
 
-EXTENSION_ID = 'pykrita_canvas_expander'
-MENU_ENTRY = 'Canvas Expander'
+EXTENSION_ID_FULL = 'pykrita_canvas_expander_full'
+MENU_ENTRY_FULL = 'Canvas Expander'
+
+EXTENSION_ID_CURRENT = 'pykrita_canvas_expander_current'
+MENU_ENTRY_CURRENT = 'Canvas Expander (Current Layer Only)'
+
+EXTENSION_ID_ALL_LAYERS = 'pykrita_canvas_expander_layers'
+MENU_ENTRY_ALL_LAYERS = 'Canvas Expander (layers Only)'
+
+EXTENSION_ID_SCALE_TO_ZOOM = 'pykrita_canvas_expander_scale_to_zoom'
+MENU_ENTRY_SCALE_TO_ZOOM = 'Canvas Expander (Scale to zoom)'
 
 class CanvasExpander(Extension):
 
@@ -17,11 +27,26 @@ class CanvasExpander(Extension):
         pass
 
     def createActions(self, window):
-        action = window.createAction(EXTENSION_ID, MENU_ENTRY, "tools/scripts")
-        # parameter 1 = the name that Krita uses to identify the action
-        # parameter 2 = the text to be added to the menu entry for this script
-        # parameter 3 = location of menu entry
-        action.triggered.connect(self.action_triggered)
+        action = window.createAction(EXTENSION_ID_FULL, MENU_ENTRY_FULL, "tools/scripts")
+        action.triggered.connect(self.action_triggered_full)
 
-    def action_triggered(self):
-        ExpanderFunction()
+        action = window.createAction(EXTENSION_ID_CURRENT, MENU_ENTRY_CURRENT, "tools/scripts")
+        action.triggered.connect(self.action_triggered_current)
+
+        action = window.createAction(EXTENSION_ID_ALL_LAYERS, MENU_ENTRY_ALL_LAYERS, "tools/scripts")
+        action.triggered.connect(self.action_triggered_layers)
+
+        action = window.createAction(EXTENSION_ID_SCALE_TO_ZOOM, MENU_ENTRY_SCALE_TO_ZOOM, "tools/scripts")
+        action.triggered.connect(self.action_triggered_scale_to_zoom)
+
+    def action_triggered_full(self):
+        ExpanderFunction(selectedLayerOnly=False, includeviewport=True)
+
+    def action_triggered_current(self):
+        ExpanderFunction(selectedLayerOnly=True, includeviewport=False)
+
+    def action_triggered_layers(self):
+        ExpanderFunction(selectedLayerOnly=False, includeviewport=False)
+
+    def action_triggered_scale_to_zoom(self):
+        ScaleToZoomFunction()
