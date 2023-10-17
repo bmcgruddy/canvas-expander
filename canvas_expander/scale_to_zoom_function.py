3ec16711002a290d17a3fb4185ef723471879ea4
bmcgruddy <rdd6afjwo@mozmail.com>
2023-10-17 13:21:47 +1100

    Fix scale to zoom so that it won't jump to the top left corner after execution.

--- canvas_expander/scale_to_zoom_function.py
+++ canvas_expander/scale_to_zoom_function.py
@@ -1,10 +1,19 @@
 from krita import Krita
-
+from krita import QMdiArea, QAbstractScrollArea
 
 def ScaleToZoomFunction():
   instance = Krita.instance()
   documement = instance.activeDocument()
   window = instance.activeWindow()
+  scroll = (
+    window.qwindow()
+    .findChild(QMdiArea)
+    .currentSubWindow()
+    .findChild(QAbstractScrollArea)
+    )
+
+  s_horizontal = scroll.horizontalScrollBar().value()
+  s_vertical = scroll.verticalScrollBar().value()
 
   c_zoom = window.activeView().canvas().zoomLevel()
   d_resolution = documement.resolution()
@@ -12,10 +21,13 @@ def ScaleToZoomFunction():
   d_height = documement.height()
   w_devicePixelRatioF = window.qwindow().devicePixelRatioF()
 
+
   _zoom = c_zoom * w_devicePixelRatioF / (d_resolution/72)
   _width = int(d_width * _zoom)
   _height = int(d_height * _zoom)
 
   documement.scaleImage(_width, _height, d_resolution, d_resolution, 'bicubic')
   window.activeView().canvas().setZoomLevel(1.0)
+  scroll.horizontalScrollBar().setValue(s_horizontal)
+  scroll.verticalScrollBar().setValue(s_vertical)
   documement.refreshProjection()
