679326c1296ffd562565f3990de4ee4ad0974022
bmcgruddy <rdd6afjwo@mozmail.com>
2023-10-18 12:24:42 +1100

    Remove unused code.

--- canvas_expander/canvas_expander.py
+++ canvas_expander/canvas_expander.py
@@ -5,25 +5,20 @@ from .action_definitions import BuildActionInstances
 
 class CanvasExpander(Extension):
 
-    def __init__(self, parent):
-        # Always initialise the superclass.
-        # This is necessary to create the underlying C++ object
-        super().__init__(parent)
+  def __init__(self, parent):
+    # Always initialise the superclass.
+    # This is necessary to create the underlying C++ object
+    super().__init__(parent)
 
-    def setup(self):
-        pass
+  def setup(self):
+    pass
 
-    def createActions(self, window):
-        def _function_constructor(operator, *args, **kwargs):
-            def _function(self):
-                return operator(*args, **kwargs)
-            return _function
+  def createActions(self, window):
+    _action_list = BuildActionInstances()
 
-        _action_list = BuildActionInstances()
+    for _action_def_instance in _action_list:
+      setattr(self, _action_def_instance.actionTriggerName, _action_def_instance.construct())
 
-        for _action_def_instance in _action_list:
-            setattr(self, _action_def_instance.actionTriggerName, _action_def_instance.construct())
-
-            action = window.createAction(_action_def_instance.actionIdentifier, _action_def_instance.actionMenuEntry, "tools/scripts")
-            action.triggered.connect(getattr(self, _action_def_instance.actionTriggerName))
+      action = window.createAction(_action_def_instance.actionIdentifier, _action_def_instance.actionMenuEntry, "tools/scripts")
+      action.triggered.connect(getattr(self, _action_def_instance.actionTriggerName))
 
