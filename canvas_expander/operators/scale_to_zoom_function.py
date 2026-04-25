f373b028cd0a619d8305b6b851c76fa1017e6170
bmcgruddy <rdd6afjwo@mozmail.com>
2026-04-26 08:45:37 +1000

    Fix scaletozoom for 5.3.1

--- canvas_expander/operators/scale_to_zoom_function.py
+++ canvas_expander/operators/scale_to_zoom_function.py
@@ -19,16 +19,12 @@ def ScaleToZoomFunction(*args, **kwargs):
   d_resolution = documement.resolution()
   d_width = documement.width()
   d_height = documement.height()
-  w_devicePixelRatioF = window.qwindow().devicePixelRatioF()
 
-
-  _zoom = c_zoom * w_devicePixelRatioF / (d_resolution/72)
-
-  if round(_zoom, 2) == 1.0:
+  if round(c_zoom, 2) == 1.0:
     return (False, "No need to scale zoom level is 100%.")
 
-  _width = int(d_width * _zoom)
-  _height = int(d_height * _zoom)
+  _width = int(d_width * c_zoom)
+  _height = int(d_height * c_zoom)
 
   documement.scaleImage(_width, _height, d_resolution, d_resolution, 'bicubic')
   window.activeView().canvas().setZoomLevel(1.0)
@@ -37,4 +33,4 @@ def ScaleToZoomFunction(*args, **kwargs):
 
   documement.refreshProjection()
 
-  return (True, f'Scaled {_zoom}')
+  return (True, f'Scaled {c_zoom:.2f}')
